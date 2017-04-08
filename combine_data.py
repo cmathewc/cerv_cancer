541be85af00da34ce4fe24cd9292629eb3ff7418
Mathew Carathedathu <cmathewc@gmail.com>
2017-04-08 13:20:01 -0700

    Edited to be able to read test images

--- combine_data.py
+++ combine_data.py
@@ -18,26 +18,35 @@ os.chdir("/Data/cerv_cancer")
 #print(os.getcwd())
 fieldnames = ['Path','Type']
 
-type1_rpath = os.path.join('additional', 'Type_1')
-type2_rpath = os.path.join('additional', 'Type_2')
-type3_rpath = os.path.join('additional', 'Type_3')
+type1_rpath = os.path.join('complete', 'Type_1')
+type2_rpath = os.path.join('complete', 'Type_2')
+type3_rpath = os.path.join('complete', 'Type_3')
 
 type1_path = os.path.join(os.getcwd(), 'Data', type1_rpath)
 type2_path = os.path.join(os.getcwd(), 'Data', type2_rpath)
 type3_path = os.path.join(os.getcwd(), 'Data', type3_rpath)
 
-ofile = open('img_key_additional.csv',"w")
+test_path = os.path.join(os.getcwd(), 'Data', 'test_small')
+
+ofile = open('img_key_test.csv',"w")
 data_writer = csv.DictWriter(ofile, fieldnames = fieldnames, delimiter = ',')
 #data_writer.writeheader()
-for file in os.listdir(type1_path):
-    if (file.endswith('.jpeg') or file.endswith('.jpg')):
-        data_writer.writerow({'Path': os.path.join(type1_path, file), 'Type':1})
-        
-
-for file in os.listdir(type2_path):
+#for file in os.listdir(type1_path):
+#    if (file.endswith('.jpeg') or file.endswith('.jpg')):
+#        data_writer.writerow({'Path': os.path.join(type1_path, file), 'Type':1})
+#        
+#
+#for file in os.listdir(type2_path):
+#    if file.endswith('.jpeg') or file.endswith('.jpg'):
+#        data_writer.writerow({'Path': os.path.join(type2_path, file), 'Type':2})
+#
+#for file in os.listdir(type3_path):
+#    if file.endswith('.jpeg') or file.endswith('.jpg'):
+#        data_writer.writerow({'Path': os.path.join(type3_path, file), 'Type':3})    
+ct = 0
+for file in os.listdir(test_path):
     if file.endswith('.jpeg') or file.endswith('.jpg'):
-        data_writer.writerow({'Path': os.path.join(type2_path, file), 'Type':2})
+        data_writer.writerow({'Path': os.path.join(test_path, file), 'Type':0})    
+        ct +=1
 
-for file in os.listdir(type3_path):
-    if file.endswith('.jpeg') or file.endswith('.jpg'):
-        data_writer.writerow({'Path': os.path.join(type3_path, file), 'Type':3})    
+ofile.close()
