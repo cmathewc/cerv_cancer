f06fb819e438a48b72951cfe297fd147612616dc
Mathew Carathedathu <cmathewc@gmail.com>
2017-04-07 23:42:33 -0700

    Edit to create csv from additional data

--- combine_data.py
+++ combine_data.py
@@ -18,15 +18,15 @@ os.chdir("/Data/cerv_cancer")
 #print(os.getcwd())
 fieldnames = ['Path','Type']
 
-type1_rpath = os.path.join('train_small2', 'Type_1')
-type2_rpath = os.path.join('train_small2', 'Type_2')
-type3_rpath = os.path.join('train_small2', 'Type_3')
+type1_rpath = os.path.join('additional', 'Type_1')
+type2_rpath = os.path.join('additional', 'Type_2')
+type3_rpath = os.path.join('additional', 'Type_3')
 
 type1_path = os.path.join(os.getcwd(), 'Data', type1_rpath)
 type2_path = os.path.join(os.getcwd(), 'Data', type2_rpath)
 type3_path = os.path.join(os.getcwd(), 'Data', type3_rpath)
 
-ofile = open('img_key.csv',"w")
+ofile = open('img_key_additional.csv',"w")
 data_writer = csv.DictWriter(ofile, fieldnames = fieldnames, delimiter = ',')
 #data_writer.writeheader()
 for file in os.listdir(type1_path):
