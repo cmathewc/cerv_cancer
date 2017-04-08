920efd76568e5bd1fa52c020dbd3a2c5c813c423
Mathew Carathedathu <cmathewc@gmail.com>
2017-04-07 23:44:32 -0700

    Optimize process of loading all the images into memory. Read as list and convert to array

--- load_img.py
+++ load_img.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import numpy as np, os, cv2, csv
+import numpy as np, os, cv2, csv, pickle
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -20,26 +20,32 @@ def load_data():
   path_3 = os.path.join(os.getcwd(),'Data','train_small2','Type_3')
   
   ct = 0
-  img_data = np.zeros((1,512,512, 3)).astype('float32')
-  img_label = np.int(0)
+  img_data = []#np.zeros((1,512,512, 3)).astype('float32')
+  img_label = []#np.int(0)
   
   csvFile = './img_key.csv'
   
   with open(csvFile) as fid:
     csvReader = csv.reader(fid)
     for row in csvReader:
-      img_data = np.vstack((img_data, cv2.imread(row[0]).reshape(1, 512, 512, 3).astype('float32')))
-      img_label = np.hstack((img_label, np.int(row[1])))
+      img_data.append(cv2.imread(row[0]).astype('float32'))
+      img_label.append(np_utils.to_categorical(np.int(row[1])-1, num_classes =3))
       ct += 1
       if np.remainder(ct, 100) == 0:
         print('Iteration_{}'.format(ct))
         
         
-  img_label_cat = np_utils.to_categorical(img_label[1:] - 1, num_classes = 3)
-  img_data = img_data[1:,:,:,:]
+  #img_label_cat = np_utils.to_categorical(img_label[1:] - 1, num_classes = 3)
+  #img_data = img_data[1:,:,:,:]
       
-  return img_data, img_label_cat, ct
+  return img_data, img_label, ct
     
 (img, label, ct) = load_data()    
 
-np.savez('img_data',img_data = img, label_data = label)
+np.savez('img_data',img_data = np.array(img), label_data = np.array(label))
+#
+#with open('img_data.txt','wb') as tf:
+#  pickle.dump(img, tf)
+#  pickle.dump(label, tf)
+#  
+#tf.close()
