8adba07dfb1d5dbf38976b2b8baf8d3e98a2f081
Mathew Carathedathu <cmathewc@gmail.com>
2017-04-09 05:12:53 -0700

    Clean up code.

--- test_pred.py
+++ test_pred.py
@@ -5,19 +5,11 @@ Created on Sat Apr  8 12:11:12 2017
 
 @author: mathew
 """
-import tensorflow as tf
 import numpy as np, os, csv, cv2
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-import sklearn
-from keras.models import Sequential, load_model
-from keras.layers import Flatten, Dense, Lambda, Dropout, Cropping2D, BatchNormalization, Activation
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
-from keras.regularizers import l2
+from keras.models import load_model
 from keras import backend as K
-from keras.utils import np_utils
-from keras.callbacks import ModelCheckpoint
+
 
 os.chdir('/Data/cerv_cancer')
 
@@ -29,10 +21,11 @@ pred_model = load_model('./output/model.h5')
 with open('img_key_test.csv','r') as ifile:
   reader = csv.reader(ifile)
   for line in reader:
-    img = cv2.imread(line[0]).astype('float32')
+#    print(line[0])
+    img = cv2.cvtColor(cv2.imread(line[0]).astype('float32'), cv2.COLOR_BGR2Lab)
     img_list.append(img)
 
-type_prob = pred_model.predict(np.array(img_list))
+type_prob_full = pred_model.predict(np.array(img_list))
     
 ifile.close()
 
